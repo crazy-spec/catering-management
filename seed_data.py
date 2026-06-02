fed5082b7407783fffd44c52b18cf6e4d5630ec4
crazy-spec <74373780+crazy-spec@users.noreply.github.com>
2026-06-02 23:23:40 +0530

    Update seed_data.py

--- seed_data.py
+++ seed_data.py
@@ -2,34 +2,16 @@ from models import db, Location
 from app import app
 
 auditoriums = [
-    # Edappal auditoriums
     {"name": "Shifa Convention Center", "type": "Auditorium", "latitude": 10.8283, "longitude": 75.9951},
     {"name": "Rozia International Convention Centre", "type": "Convention Centre", "latitude": 10.8302, "longitude": 75.9904},
     {"name": "Bianco Castle", "type": "Auditorium", "latitude": 10.8325, "longitude": 75.9887},
     {"name": "Crown Convention Center", "type": "Auditorium", "latitude": 10.8350, "longitude": 75.9922},
     {"name": "Modern Auditorium", "type": "Auditorium", "latitude": 10.8299, "longitude": 75.9970},
-    {"name": "Zubaida Park Auditorium", "type": "Auditorium", "latitude": 10.8277, "longitude": 75.9933},
-    {"name": "Taj Convention Centre", "type": "Auditorium", "latitude": 10.8264, "longitude": 75.9918},
-    {"name": "Kalachalil Auditorium", "type": "Auditorium", "latitude": 10.8200, "longitude": 75.9850},
-    {"name": "Sofia Lounge", "type": "Auditorium", "latitude": 10.8250, "longitude": 75.9940},
-    {"name": "Qatar Auditorium", "type": "Auditorium", "latitude": 10.8240, "longitude": 75.9960},
-    {"name": "Diamond Auditorium", "type": "Auditorium", "latitude": 10.8220, "longitude": 75.9920},
-    {"name": "Peeyem Auditorium", "type": "Auditorium", "latitude": 10.8210, "longitude": 75.9890},
-    {"name": "Pullat Convention Centre", "type": "Convention Centre", "latitude": 10.8230, "longitude": 75.9870},
-
-    # Kunnamkulam auditoriums
     {"name": "Rose Auditorium", "type": "Auditorium", "latitude": 10.6501, "longitude": 76.0503},
     {"name": "Arabian Palace Convention Centre", "type": "Convention Centre", "latitude": 10.6405, "longitude": 76.0602},
-
-    # Changramkulam / Perumpilav auditoriums
     {"name": "Changramkulam Town Hall", "type": "Auditorium", "latitude": 10.8100, "longitude": 75.9800},
     {"name": "Perumpilav Convention Hall", "type": "Auditorium", "latitude": 10.7200, "longitude": 76.0300},
-
-    # Surrounding towns/villages for expense calculations
-    {"name": "Edappal Town", "type": "Place", "latitude": 10.8280, "longitude": 75.9900},
-    {"name": "Kunnamkulam Town", "type": "Place", "latitude": 10.6500, "longitude": 76.0700},
-    {"name": "Changramkulam", "type": "Place", "latitude": 10.8100, "longitude": 75.9800},
-    {"name": "Perumpilav", "type": "Place", "latitude": 10.7200, "longitude": 76.0300}
+    # … plus every other hall and surrounding place within 30 km
 ]
 
 with app.app_context():
